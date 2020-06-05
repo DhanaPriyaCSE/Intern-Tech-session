65eb25f1acb2150bc2e0e791c3c9273717a7d786
DhanaPriyaCSE <yaspriya958@gmail.com>
2020-06-05 09:15:52 +0530

    Getting Nested Elements in list

---
+++ nested_Elements/getting_Element.py
@@ -0,0 +1,15 @@
+
+
+user_List=["abc",[2,3,("xyz","the","avengers")],1,2,3]
+print(user_List[1][2][2])
+
+#Prints avengers
+
+"""
+for user_List_Elements in user_List:
+    for nested_element in user_List_Elements:
+            print(nested_element)
+            if nested_element=="avengers":
+                print(nested_Element)
+            
+"""
