282c928ee18169fd7a5eba0b9b484f96dc0da6e4
DhanaPriyaCSE <yaspriya958@gmail.com>
2020-06-17 09:27:59 +0530

    validate input in pattern program

--- assignment_2/pattern_pogram.py
+++ assignment_2/pattern_pogram.py
@@ -1,41 +1,58 @@
 
-no_of_rows = int(input("Enter number of rows"))
+no_of_rows = int(input("Enter odd number of row "))
 
-for iter_val in range (0, no_of_rows):
-    if iter_val %2!=0:
-        for inner_val in range(0, iter_val ):
-            print("*", end=' ')
-        print("\r")
+if no_of_rows%2!=0:
+    for iter_val in range (0, no_of_rows):
+        if iter_val %2!=0:
+            for inner_val in range(0, iter_val ):
+                print("*", end=' ')
+            print("\r")
+            
+    for iter_val  in range (no_of_rows, 0,-1):
+        if iter_val %2!=0:
+            for inner_val in range(0, iter_val ):
+                print("*", end=' ')
+            print("\r")
+else:
+    print("Enter the valid input in odd numbers")
         
-for iter_val  in range (no_of_rows, 0,-1):
-    if iter_val %2!=0:
-        for inner_val in range(0, iter_val ):
-            print("*", end=' ')
-        print("\r")
-    
 #output1
 """
 Enter number of rows 5
-* 
-* * * 
-* * * * * 
-* * * 
+* 
+
+* * * 
+
+* * * * * 
+
+* * * 
+
 *
 """
 
 #output 2
 """
 Enter number of rows 11
-* 
-* * * 
-* * * * * 
-* * * * * * * 
-* * * * * * * * * 
-* * * * * * * * * * * 
-* * * * * * * * * 
-* * * * * * * 
-* * * * * 
-* * * 
+* 
+
+* * * 
+
+* * * * * 
+
+* * * * * * * 
+
+* * * * * * * * * 
+
+* * * * * * * * * * * 
+
+* * * * * * * * * 
+
+* * * * * * * 
+
+* * * * * 
+
+* * * 
+
 * 
 
 """
