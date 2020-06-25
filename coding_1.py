93817822933d9875817d9cae4255795a3ff30cc3
DhanaPriyaCSE <yaspriya958@gmail.com>
2020-06-25 17:18:35 +0530

    simplified code method2

--- coding_1.py
+++ coding_1.py
@@ -1,4 +1,11 @@
+
+#method1
+
 number=input("Enter the number")
 list_num=list(number)
 result=reversed(sorted(list_num))
 print(''.join(result))
+
+#method2
+
+print(''.join(reversed(sorted(list(input())))))
