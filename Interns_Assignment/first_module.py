fdd76920efa2780db14b9ccb736f03b1e7bc4e1f
DhanaPriyaCSE <yaspriya958@gmail.com>
2020-07-10 21:47:12 +0530

    python modules

---
+++ Interns_Assignment/first_module.py
@@ -0,0 +1,11 @@
+def module_fun(name):
+    print("Hi,This is "+name+".")
+x=10    
+class my_self:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def print_numbers(self):
+        print("x:" + self.x)
+        print("y:"+self.y)
---
+++ Interns_Assignment/second_module.py
@@ -0,0 +1,4 @@
+import first_module
+first_module.module_fun("priya")
+class_var= first_module.my_self('10','20')
+class_var.print_numbers()
