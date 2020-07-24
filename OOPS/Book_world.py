fa764b4c81da191e1d2e410c8fa89d2f24f84cfd
DhanaPriyaCSE <yaspriya958@gmail.com>
2020-07-24 07:21:26 +0530

    book world problem

---
+++ OOPS/Book_world.py
@@ -0,0 +1,73 @@
+class Author:
+    def __init__(self,author_name,author_age,author_nationality):
+        self.author_name=author_name
+        self.author_age=author_age
+        self.author_nationality=author_nationality
+
+class Book:
+    def __init__(self,book_name,book_price,author_name,author_age,author_nationality):
+        self.book_name=book_name
+        self.book_price=book_price
+        self.author=Author(author_name,author_age,author_nationality)
+
+def book_total_price(books):
+        total_price=0
+        for book in books:
+            total_price=total_price+book.book_price
+        return total_price
+
+def total_number_of_book(books,given_author):
+     book_count=0
+     for book in books:
+         if book.author.author_name==given_author:
+             book_count+=1     
+     return(book_count)
+
+def affortable_price_books(books):
+    print("Book name \t Author Name")
+    for book in books:
+        if book.book_price <1000:
+            print(book.book_name+"\t\t "+book.author.author_name)
+
+def print_author_name(books):
+    author=[]
+    for book in books:
+        author.append(book.author.author_name)
+    print(' ,'.join( author))
+
+
+
+python = Book("Python",999,"Guido van rossum",64,"Dutch")
+scala = Book("scala",1500,"Martin Odersky",61,"German")
+java = Book("Java",800,"James Gosling",65,"Canadian")
+c = Book("C",1999,"Dennis Ritche",79,"American")
+c_plus_plus = Book("C++",1499,"Bjarne Stroustrup",69,"Danish")
+generic_java = Book("Generic Java",599,"Martin Odersky",61,"German")
+
+books =[python,scala,c,c_plus_plus,generic_java]
+
+print("Total price of Books:",book_total_price(books))
+
+print("\nAuthor Names:")
+print_author_name(books)
+
+given_author=input("\nEnter the author name to get number of books written by author:")
+
+print("\nTotal number of book written by "+given_author+" is :"+str(total_number_of_book(books,given_author)))
+
+affortable_price_books(books)
+
+"""
+Total price of Books: 6596
+
+Author Names:
+Guido van rossum ,Martin Odersky ,Dennis Ritche ,Bjarne Stroustrup ,Martin Odersky
+
+Enter the author name to get number of books written by author:Guido van rossum
+
+Total number of book written by Guido van rossum is :1
+Book name 	 Author Name
+Python		 Guido van rossum
+Generic Java	 Martin Odersky
+"""
+
---
+++ OOPS/icream_updated.py
@@ -0,0 +1,59 @@
+class IceCream:
+    def __init__(self,icecream_type,icecream_flavour,icecream_toppings):
+        self.icecream_type=icecream_type
+        self.icecream_flavour=icecream_flavour
+        self.icecream_toppings=icecream_toppings
+    
+    def print_menu(self):
+        print("_____IceCream Menu_______\n")
+        print("______--****--_______\n")
+        for type_ice in self.icecream_type:
+            print("Type of IceCreams:",type_ice)
+            for flavour in self.icecream_flavour:                 
+                 print(flavour,(int(self.icecream_flavour[flavour]))+(int(self.icecream_type[type_ice])))
+            print("______--****--_______\n")
+                 
+        print("---***Ice Cream Toppings:***---")
+        for topping in self.icecream_toppings:
+            print(topping,self.icecream_toppings[topping])
+        print("______--****--_______\n")
+
+
+def total_cost(itype,iflavour,itopping,quantity,i):
+    i_type=0
+    i_flavor=0
+    i_topping=0
+    total_cost=0
+    if itopping!='none':
+       if itype in i.icecream_type and iflavour in i.icecream_flavour and itopping in i.icecream_toppings:
+          total_cost=(i.icecream_type[itype]+i.icecream_flavour[iflavour]+i.icecream_toppings[itopping])*quantity
+    else:
+        if itype in i.icecream_type and iflavour in i.icecream_flavour:
+          total_cost=(i.icecream_type[itype]+i.icecream_flavour[iflavour])*quantity
+    print("The total cost is:",total_cost)
+
+
+
+icecream_type={'stick':60,'cone':40,'cup':20}
+icecream_flavour={'chocolate':60,'venila':50,'strawberry':30}
+icecream_toppings={'choco chips':30,'caramel':40,'nuts':20}
+        
+ice=IceCream(icecream_type,icecream_flavour,icecream_toppings)
+ice.print_menu()
+
+itype=input("Enter the type of icream:")
+iflavour=input("Enter the flavour of icream:")
+
+if iflavour=='chocolate':
+   itopping=input("Enter the Topping of icream:")
+else:
+    itopping='none'
+
+quantity=int(input("Enter the Quantity:"))
+
+total_cost(itype,iflavour,itopping,quantity,ice)
+
+
+
+
+                        
